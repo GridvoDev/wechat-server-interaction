9e34f6457fd2d978c738d16673926e907bdf5679
Johnlinmadan <772181827@qq.com>
2017-01-11 21:30:57 +0800

    添加k8s自动部署功能

---
+++ k8s/dev/install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+kubectl get svc | grep -q wechat-server-interaction
+if [ "$?" == "1" ];then
+	kubectl create -f wechat_server_interaction-service.yaml --record
+	kubectl get svc | grep -q wechat-server-interaction
+	if [ "$?" == "0" ];then
+		echo "wechat_server_interaction-service install success!"
+	else
+		echo "wechat_server_interaction-service install fail!"
+	fi
+else
+	echo "wechat_server_interaction-service is exist!"
+fi
+kubectl get pods | grep -q wechat-server-interaction
+if [ "$?" == "1" ];then
+	kubectl create -f wechat_server_interaction-deployment.yaml --record
+	kubectl get pods | grep -q wechat-server-interaction
+	if [ "$?" == "0" ];then
+		echo "wechat_server_interaction-deployment install success!"
+	else
+		echo "wechat_server_interaction-deployment install fail!"
+	fi
+else
+	kubectl delete -f wechat_server_interaction-deployment.yaml
+	kubectl get pods | grep -q wechat-server-interaction
+	while ( "$?" == "1" )
+	do
+	kubectl create -f wechat_server_interaction-deployment.yaml --record
+	kubectl get pods | grep -q wechat-server-interaction
+	if [ "$?" == "0" ];then
+		echo "wechat_server_interaction-deployment update success!"
+	else
+		echo "wechat_server_interaction-deployment update fail!"
+	fi
+	done
+fi
